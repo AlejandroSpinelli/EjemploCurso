7653c147f341030a646735bb704921d4219a86b8
Alejandro Spinelli <gabrielalejandrospinelli@gmail.com>
2024-02-27 20:42:22 -0300

    TPFinal

---
+++ TPFinalNivel_Spinelli/Dominio/Marca.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Marca
+    {
+        public override string ToString()
+        {
+            return Descripcion;
+        }
+
+        public int Id { get; set; }
+
+        public string Descripcion { get; set; }
+    }
+}
---
+++ TPFinalNivel_Spinelli/Negocio/AccesoADatos.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Negocio
+{
+    public class AccesoADatos
+    {
+        SqlConnection conexion;
+        SqlCommand comando;
+        SqlDataReader lector;
+
+        public SqlDataReader Lector
+        {
+            get { return lector; }
+        }
+
+        public AccesoADatos()
+        {
+            conexion=new SqlConnection("server=.\\SQLEXPRESS;database=CATALOGO_DB;integrated security=true");
+            comando = new SqlCommand();
+        }
+
+        public void establecerConsulta(string consulta)
+        {
+            comando.CommandType=System.Data.CommandType.Text;
+            comando.CommandText = consulta;
+        }
+
+        public void establecerlectura()
+        {
+            comando.Connection = conexion;
+            conexion.Open();
+            lector = comando.ExecuteReader();
+        }
+
+        public void cerrarconexion()
+        {
+            if (Lector != null) 
+                Lector.Close();
+            conexion.Close();
+        }
+
+        public void ejecutaraccion()
+        {   comando.Connection = conexion;
+            try
+            {
+                conexion.Open();
+                comando.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public void setearparametros(string nombre,object valor)
+        {
+            comando.Parameters.AddWithValue(nombre,valor);
+        }
+    }
+}
